51d1f511aeb5da01952d871951d3d53382c5bf3d
Tres Seaver <tseaver@palladion.com>
2006-04-15 16:18:58 +0000

    'package_dir' must be a relative path.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ except ImportError, e:
     from distutils.core import setup, Extension
 
 setup(name='zope.component',
-      version='3.0',
+      version='3.3-dev',
       url='http://svn.zope.org/zope.component',
       license='ZPL 2.1',
       description='component',
@@ -37,7 +37,7 @@ setup(name='zope.component',
                 'zope.component.bbb',
                 'zope.component.bbb.tests',
                ],
-      package_dir = {'': os.path.join(os.path.dirname(__file__), 'src')},
+      package_dir = {'': 'src'},
 
       namespace_packages=['zope',],
       tests_require = ['zope.testing'],
