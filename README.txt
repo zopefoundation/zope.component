2bec3f4f235e19add8ee52519f9dae2585ba153c
Jim Fulton <jim@zope.com>
2007-02-21 16:04:04 +0000

    Updated release info.

--- README.txt
+++ README.txt
@@ -6,9 +6,6 @@ This package represents the core of the Zope Component Architecture.
 Together with the 'zope.interface' package, it provides facilities for
 defining, registering and looking up components.
 
-Please see 'src/zope/component/README.txt' for a more detailed explanation
-of the component architecture.
-
 .. contents::
 
 Releases
--- setup.py
+++ setup.py
@@ -25,44 +25,57 @@ def read(*rnames):
 
 
 name = 'zope.component'
-setup(name=name,
-      version='3.4dev',
-      url='http://www.python.org/pypi/'+name,
-      license='ZPL 2.1',
-      description='Core of the Zope Component Architecture',
-      author='Zope Corporation and Contributors',
-      author_email='zope3-dev@zope.org',
-      long_description="This package, together with `zope.interface`,"
-                       "provides facilities for defining, registering"
-                       "and looking up components.  There are two basic"
-                       "kinds of components: adapters and utilities.",
-      
-      packages = find_packages('src'),
-      package_dir = {'': 'src'},
+setup(
+    name=name,
+    version='3.4dev',
+    url='http://www.python.org/pypi/'+name,
+    license='ZPL 2.1',
+    description='Core of the Zope Component Architecture',
+    author='Zope Corporation and Contributors',
+    author_email='zope3-dev@zope.org',
+    long_description=(
+        read('README.txt')
+        + '\n' +
+        'Detailed Documentation\n'
+        '**********************\n'
+        + '\n' +
+        read('src', 'zope', 'component', 'README.txt')
+        + '\n' +
+        read('src', 'zope', 'component', 'event.txt')
+        + '\n' +
+        read('src', 'zope', 'component', 'registry.txt')
+        + '\n' +
+        read('src', 'zope', 'component', 'socketexample.txt')
+        + '\n' +
+        'Download\n'
+        '**********************\n'
+        ),
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
 
-      namespace_packages=['zope',],
-      tests_require = ['zope.testing'],
-      install_requires=['zope.deprecation',
-                        'zope.interface',
-                        'zope.deferredimport',
-                        'zope.event',
-                        'setuptools',
-                       ],
-      include_package_data = True,
-      zip_safe = False,
-      extras_require = dict(
-          service = ['zope.exceptions'],
-          zcml = ['zope.configuration', 'zope.security', 'zope.proxy',
-                  'zope.i18nmessageid',
-                  #'zope.location', # should be depenency of zope.security
-                  ],
-          test = ['zope.testing',
-                  'ZODB3',
-                  #'zope.configuration', 'zope.security', 'zope.proxy',
-                  #'zope.i18nmessageid',
-                  #'zope.location', # should be depenency of zope.security
-                  ],
-          hook = ['zope.hookable'],
-          persistentregistry = ['ZODB3'],
-          ),
-      )
+    namespace_packages=['zope',],
+    tests_require = ['zope.testing'],
+    install_requires=['zope.deprecation',
+                      'zope.interface',
+                      'zope.deferredimport',
+                      'zope.event',
+                      'setuptools',
+                      ],
+    include_package_data = True,
+    zip_safe = False,
+    extras_require = dict(
+        service = ['zope.exceptions'],
+        zcml = ['zope.configuration', 'zope.security', 'zope.proxy',
+                'zope.i18nmessageid',
+                #'zope.location', # should be depenency of zope.security
+                ],
+        test = ['zope.testing',
+                'ZODB3',
+                #'zope.configuration', 'zope.security', 'zope.proxy',
+                #'zope.i18nmessageid',
+                #'zope.location', # should be depenency of zope.security
+                ],
+        hook = ['zope.hookable'],
+        persistentregistry = ['ZODB3'],
+        ),
+    )
