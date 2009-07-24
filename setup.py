b483847ac98926a39ffd3c8820367a67d31efe1d
Stephan Richter <srichter@gmail.com>
2009-07-24 08:59:43 +0000

    version bump.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*rnames):
 
 setup(
     name='zope.component',
-    version = '3.7.1',
+    version = '3.8.0dev',
     url='http://pypi.python.org/pypi/zope.component',
     license='ZPL 2.1',
     description='Zope Component Architecture',
