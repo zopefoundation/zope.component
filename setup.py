6ae8b47a8878c532435a0a51188cc4c6d44a5cd1
Martijn Faassen <faassen@startifact.com>
2010-01-21 18:38:26 +0000

    Back to development: 3.9.1

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def read(*rnames):
 
 setup(
     name='zope.component',
-    version = '3.9.0',
+    version = '3.9.1dev',
     url='http://pypi.python.org/pypi/zope.component',
     license='ZPL 2.1',
     description='Zope Component Architecture',
