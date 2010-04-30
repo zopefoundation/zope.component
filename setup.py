312d398f6931bfbb1f7b1053c64fba3f39395c3d
Hanno Schlichting <hanno@hannosch.eu>
2010-04-30 20:35:17 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def read(*rnames):
 
 setup(
     name='zope.component',
-    version = '3.9.4',
+    version = '3.9.5dev',
     url='http://pypi.python.org/pypi/zope.component',
     license='ZPL 2.1',
     description='Zope Component Architecture',
