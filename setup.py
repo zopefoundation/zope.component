e697304c5a175f68c112484e2abd71d0c1eb1479
Wolfgang Schnerring <wosc@wosc.de>
2011-11-16 15:07:11 +0000

    Back to development: 3.12.1

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def read(*rnames):
 
 setup(
     name='zope.component',
-    version='3.12.0',
+    version='3.12.1dev',
     url='http://pypi.python.org/pypi/zope.component',
     license='ZPL 2.1',
     description='Zope Component Architecture',
