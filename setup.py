eb9aae144acf6cd80715eb6aa5706cb64464b5a5
Tres Seaver <tseaver@palladion.com>
2012-07-02 12:40:21 +0000

    svb

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ def read(*rnames):
 
 setup(
     name='zope.component',
-    version='4.0.0',
+    version='4.0.1dev',
     url='http://pypi.python.org/pypi/zope.component',
     license='ZPL 2.1',
     description='Zope Component Architecture',
