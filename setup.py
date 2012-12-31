45e1936dba476237158ff6f0313b9bec3a6f9183
Tres Seaver <tseaver@palladion.com>
2012-12-31 18:06:24 +0000

    Add 'Framework :: Zope3' classifier.

--- setup.py
+++ setup.py
@@ -98,6 +98,7 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
+        "Framework :: Zope3",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     namespace_packages=['zope',],
