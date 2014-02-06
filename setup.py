a974b9c66d83591336daea3f3a00b2592177f361
Stephan Richter <stephan.richter@gmail.com>
2014-02-05 22:22:10 -0500

    Back to development: 4.2.1

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ def read(*rnames):
 
 setup(
     name='zope.component',
-    version='4.2.0',
+    version='4.2.1.dev0',
     url='http://pypi.python.org/pypi/zope.component',
     license='ZPL 2.1',
     description='Zope Component Architecture',
