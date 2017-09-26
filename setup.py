0d333a42da506076c98cbf37f3fd041fc7399879
Jason Madden <jamadden@gmail.com>
2017-09-26 06:18:09 -0500

    Back to development: 4.4.2

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ def read(*rnames):
 
 setup(
     name='zope.component',
-    version='4.4.1',
+    version='4.4.2.dev0',
     url='https://github.com/zopefoundation/zope.component',
     license='ZPL 2.1',
     description='Zope Component Architecture',
