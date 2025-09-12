7b03eb9bfa08b586fdbdf87d5dc6556f3cd91892
Michael Howitz <icemac@gmx.net>
2025-09-12 08:59:01 +0200

    Preparing release 7.0

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ def read(*rnames):
 
 setup(
     name='zope.component',
-    version='7.0.dev0',
+    version='7.0',
     url='https://github.com/zopefoundation/zope.component',
     project_urls={
         'Documentation': 'https://zopecomponent.readthedocs.io/',
